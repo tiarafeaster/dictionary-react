55bc2fdd0bcabe1d1ac0cb6728276c6c42009a2b
Tiara Feaster <tfeaster21@gmail.com>
2021-07-27 18:22:57 -0400

    rendered the meanings of the words searched

---
+++ src/Meaning.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h4>{props.meaning.partOfSpeech}</h4>
+      {props.meaning.definitions.map((definition, index) => {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Meaning;
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "./Results.css";
+import Meaning from "./Meaning";
 
 function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
-      <div>
+      <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.meanings.map((meaning, index) => {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
