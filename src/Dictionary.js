63f17c637c67fdac0629bbaee14a697aafb08208
Tiara Feaster <tfeaster21@gmail.com>
2021-07-27 17:07:07 -0400

    added axios and integrated dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword} definition`);
+
+    //documentation: https://dictionaryapi.dev/
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}
+    `;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
 
   function handleKeywordChange(event) {
@@ -17,6 +27,7 @@ function Dictionary() {
     <div>
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <button>Search</button>
       </form>
     </div>
   );
