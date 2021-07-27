fc49c3f2f0d1a304d3eec6bdbf3ef79434198488
Tiara Feaster <tfeaster21@gmail.com>
2021-07-27 16:09:15 -0400

    added border to make sure bootstrap was installed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      hey this is my react dictionary app<h3></h3>
+    <div className="App container ">
+      <div className="row">
+        <div className="col">
+          <h2>React Dictionary App</h2>
+        </div>
+      </div>
+      <h3>this is my react dictionary app</h3>
+      <p>
+        This react dictionary app is{" "}
+        <a
+          href="https://www.github.com/tiarafeaster/dictionary-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open source
+        </a>{" "}
+        and coded by{" "}
+        <a href="http://www.tiarajfeaster.com" target="_blank" rel="noreferrer">
+          Tiara Feaster
+        </a>
+      </p>
     </div>
   );
 }
